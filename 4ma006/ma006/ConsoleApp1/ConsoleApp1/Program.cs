9e99e3ce008f2d25e1d3809500f6ebabea52b141
hardilundavia <50069843+hardilundavia@users.noreply.github.com>
2019-12-18 10:31:43 +0530

    Add files via upload

---
+++ 4ma006/ma006/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Area_cal a = new Area_cal();
+            Console.WriteLine("Enter radious : ");
+            double ra = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area of circle : "+a.Area(ra));
+
+            Console.WriteLine("Enter base  : ");
+            double base1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter height  : ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area of circle : " + a.Area(base1 , height));
+
+            Console.WriteLine("Enter length  : ");
+            int length = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter width  : ");
+            int width = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Area of circle : " + a.Area(length, width));
+
+            Console.WriteLine("Enter side : ");
+            int side = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of circle : " + a.Area(side));
+        }
+    }
+    class Area_cal
+    {
+        public const double PI = 3.14;
+        public double Area(double r)
+        {
+            return PI * r *  r;
+        }
+        public double Area(double b, double h)
+        {
+            return 0.5 * b * h;
+        }
+        public int Area(int l, int w)
+        {
+            return l * w;
+        }
+        public int Area(int a)
+        {
+            return a * a;
+        }
+    }
+
+}
+
---
+++ 4ma006/ma006/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string[] str = new string[100];
+            Console.WriteLine(" Enter how many strings you want to store : ");
+            int no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("strings you entered : " + no);
+
+            for (i = 0; i < no; i++)
+            {
+                Console.Write("\nEnter string" +i+" : ");
+                str[i] = Console.ReadLine();
+            }
+            Console.WriteLine("strings in array \n");
+            for (i = 0; i < no; i++)
+            {
+                Console.WriteLine("\n string : " +str[i]);
+                
+            }
+         
+
+                Console.ReadKey();
+        }
+    }
+}
